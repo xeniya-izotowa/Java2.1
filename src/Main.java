c03c5552c152bba31caccdcbc08a19346434cd85
xeniya <xeniia.izotowa@gmail.com>
2021-07-13 12:06:16 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int current_balance = 2000000000;
+        int transfer = 500000000;
+        int result = current_balance + transfer;
+        System.out.println(result);
+    }
+}
